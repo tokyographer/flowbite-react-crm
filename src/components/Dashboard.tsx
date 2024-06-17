9be0a70e8d445c5e0aada1549160a15a38fc4337
Anthony T <tokyographer@gmail.com>
2024-06-17 11:36:16 +0200

    Created components folder and dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      {/* Dashboard content goes here */}
+    </div>
+  );
+}
+
+export default Dashboard;
